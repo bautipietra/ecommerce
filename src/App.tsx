07e31bb1bfce9b8a3d947c7c099ee0059d5014bf
Bautista Pietraroia <bautistapietraroia@gmail.com>
2023-02-11 09:52:23 -0300

    Add notifications

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Toaster } from 'react-hot-toast'
 import { Routes, Route } from 'react-router-dom'
 import Footer from './components/Footer'
 import Nav from './components/Nav'
@@ -7,6 +8,7 @@ function App() {
 
   return (
     <div className='w-full overflow-x-hidden'>
+      <Toaster position="top-center" />
       <Nav></Nav>
       <Routes>
         <Route path="/" element={<Home></Home>} />
