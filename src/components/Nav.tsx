562cd9341643e1e7179cc177c21c48222e50ae5a
Bautista Pietraroia <bautistapietraroia@gmail.com>
2023-02-11 13:13:07 -0300

    update links on nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -19,10 +19,9 @@ const Nav = () => {
           </a>
           {/* Nav Links */}
           <ul className="hidden md:flex px-4 mx-auto font-semibold font-heading space-x-12">
-            <li><a className="hover:text-zinc-900" href="#">Home</a></li>
-            <li><Link className="hover:text-zinc-900" to="/products">Category</Link></li>
-            <li><a className="hover:text-zinc-900" href="#">Collections</a></li>
-            <li><a className="hover:text-zinc-900" href="#">Contact Us</a></li>
+            <li><a className="hover:text-zinc-900" href="/">Home</a></li>
+            <li><Link className="hover:text-zinc-900" to="/products">Sneakers</Link></li>
+            <li><a className="hover:text-zinc-900" href="/testimonial">Testimonial</a></li>
           </ul>
           {/* Header Icons */}
           <div className="hidden xl:flex items-center space-x-5 items-center">
@@ -42,7 +41,7 @@ const Nav = () => {
               </span>
             </a>
             {/* Sign In / Register      */}
-            <a className="flex items-center hover:text-zinc-900" href="#">
+            <a className="flex items-center hover:text-zinc-900" href="/login">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 hover:text-zinc-900" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
               </svg>
@@ -60,7 +59,7 @@ const Nav = () => {
             </span>
           </span>
         </a>
-        <a className="navbar-burger self-center mr-12 xl:hidden" href="#">
+        <a className="navbar-burger self-center mr-12 xl:hidden" href="">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 hover:text-zinc-900" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
           </svg>
