b32d94430136cfe9ca98a39068c79ab625fd749f
Bautista Pietraroia <bautistapietraroia@gmail.com>
2023-03-15 23:32:39 -0300

    add material tailwind to tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,12 @@
-module.exports = {
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
   darkMode: 'class',
   content: [
     "./src/**/*.{js,ts,jsx,tsx}",
     "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "path-to-your-node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "path-to-your-node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
@@ -27,5 +31,5 @@ module.exports = {
   plugins: [
     require('@tailwindcss/forms')
   ],
-}
+})
 
