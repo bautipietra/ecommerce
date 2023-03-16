e427a41b77a67e73af904d9ae043cc577fa37cc8
Bautista Pietraroia <bautistapietraroia@gmail.com>
2023-03-15 23:35:15 -0300

    fix responsive nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -18,11 +18,11 @@ const Nav = () => {
           {/* Nav Links */}
           <ul className="hidden md:flex px-4 mx-auto font-semibold font-heading space-x-12">
             <li><Link className="hover:text-zinc-900" to="/">Home</Link></li>
-            <li><Link className="hover:text-zinc-900" to="/products">Sneakers</Link></li>
-            <li><Link className="hover:text-zinc-900" to="/reviews">Testimonial</Link></li>
+            <li><Link className="hover:text-zinc-900" to="/sneakers">Sneakers</Link></li>
+            <li><Link className="hover:text-zinc-900" to="/faq">FAQ</Link></li>
           </ul>
           {/* Header Icons */}
-          <div className="hidden xl:flex items-center space-x-5 items-center">
+          <div className="hidden md:flex items-center space-x-5 items-center">
             <Link className="flex items-center hover:text-zinc-900" to="#">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
@@ -42,7 +42,7 @@ const Nav = () => {
           </div>
         </div>
         {/* Responsive navbar */}
-        <Link className="xl:hidden flex mr-6 items-center" to="#">
+        <Link className="md:hidden flex mr-6 items-center" to="#">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 hover:text-zinc-900" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
           </svg>
@@ -52,7 +52,7 @@ const Nav = () => {
             </span>
           </span>
         </Link>
-        <Link className="navbar-burger self-center mr-12 xl:hidden" to="">
+        <Link className="navbar-burger self-center mr-12 md:hidden" to="">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 hover:text-zinc-900" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
           </svg>
