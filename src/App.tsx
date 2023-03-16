d23215752d27aea2a2b7a9725e1bc01597d99e79
Bautista Pietraroia <bautistapietraroia@gmail.com>
2023-03-15 23:34:12 -0300

    add new routes in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { Suspense } from 'react'
 import { Toaster } from 'react-hot-toast'
 import { Routes, Route } from 'react-router-dom'
 import Footer from './components/Footer'
 import Nav from './components/Nav'
+import Error from './pages/Error/Error'
+import Faq from './pages/Faq/Faq'
 import Home from './pages/Home/Home'
 import Login from './pages/Login/Login'
 import Register from './pages/Register/Register'
+import Sneakers from './pages/Sneakers/Sneakers'
 
 function App() {
 
@@ -22,11 +26,16 @@ function App() {
         }
       />
       <Nav></Nav>
-      <Routes>
-        <Route path="/" element={<Home></Home>} />
-        <Route path="/login" element={<Login></Login>} />
-        <Route path="/register" element={<Register></Register>} />
-      </Routes>
+      <Suspense fallback={null}>
+        <Routes>
+          <Route path="/" element={<Home></Home>} />
+          <Route path="/login" element={<Login></Login>} />
+          <Route path="/register" element={<Register></Register>} />
+          <Route path="/sneakers" element={<Sneakers></Sneakers>} />
+          <Route path="/faq" element={<Faq></Faq>} />
+          <Route path="*" element={<Error></Error>} />
+        </Routes>
+      </Suspense>
       <Footer></Footer>
     </div>
   )
