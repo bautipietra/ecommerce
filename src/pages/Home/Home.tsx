a76e3a67f01b643f425c1a2a0163197067f4d386
Bautista Pietraroia <bautistapietraroia@gmail.com>
2023-03-15 23:36:22 -0300

    change anchors to Link in home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -11,9 +11,9 @@ export default function Home() {
           <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div className="grid items-center grid-cols-1 sm:gap-12 lg:grid-cols-2">
               <div className='grid mb-8 sm:py-16'>
-                <h1 className="mt-4 text-4xl font-bold text-black text-center lg:mt-8 lg:text-start  sm:text-6xl xl:text-8xl">Great way to boost  your <span className="text-transparent bg-clip-text bg-gradient-to-r from-zinc-600 to-zinc-400">style!</span></h1>
+                <h1 className="mt-4 text-4xl font-bold text-black text-center lg:mt-8 lg:text-start  sm:text-6xl xl:text-8xl">Great way to boost  your <span className="text-transparent bg-clip-text bg-gradient-to-r from-gray-600 to-gray-400">style!</span></h1>
                 {/* <p className="mt-4 text-base text-black lg:mt-8 sm:text-xl">Grow your career fast with right mentor.</p> */}
-                <Link to="/products" className="justify-self-center lg:justify-self-start inline-flex items-center px-6 py-4 mt-8 font-semibold text-white transition-all duration-200 bg-black lg:mt-16 hover:bg-zinc-900 focus:bg-zinc-900" role="button">
+                <Link to="/sneakers" className="justify-self-center lg:justify-self-start inline-flex items-center px-6 py-4 mt-8 font-semibold text-white transition-all duration-200 bg-black lg:mt-16 hover:bg-zinc-900 focus:bg-zinc-900" role="button">
                   Shop now
                   <svg className="w-6 h-6 ml-8 -mr-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z" />
@@ -31,12 +31,12 @@ export default function Home() {
       <div className='bg-black py-8 w-full text-white'>
         <div className="relative flex overflow-x-hidden">
           <div className="py-12 animate-marquee whitespace-nowrap">
-            <span className="text-2xl font-bold text-white mx-20 lg:text-6xl">DISCOVER LIMITED <span className="text-transparent bg-clip-text bg-gradient-to-r from-zinc-600 to-zinc-400">SNEAKERS</span>  WITHOUT LIMITATION</span>
-            <span className="text-2xl font-bold text-white mx-20 lg:text-6xl">DISCOVER LIMITED  <span className="text-transparent bg-clip-text bg-gradient-to-r from-zinc-600 to-zinc-400">SNEAKERS</span> WITHOUT LIMITATION</span>
+            <span className="text-2xl font-bold text-white mx-20 lg:text-6xl">DISCOVER LIMITED <span className="text-transparent bg-clip-text bg-gradient-to-r from-gray-600 to-gray-400">SNEAKERS</span>  WITHOUT LIMITATION</span>
+            <span className="text-2xl font-bold text-white mx-20 lg:text-6xl">DISCOVER LIMITED  <span className="text-transparent bg-clip-text bg-gradient-to-r from-gray-600 to-gray-400">SNEAKERS</span> WITHOUT LIMITATION</span>
           </div>
           <div className="absolute top-0 py-12 animate-marquee2 whitespace-nowrap">
-            <span className="text-2xl font-bold text-white mx-20 lg:text-6xl">DISCOVER LIMITED <span className="text-transparent bg-clip-text bg-gradient-to-r from-zinc-600 to-zinc-400">SNEAKERS</span>  WITHOUT LIMITATION</span>
-            <span className="text-2xl font-bold text-white mx-20 lg:text-6xl">DISCOVER LIMITED <span className="text-transparent bg-clip-text bg-gradient-to-r from-zinc-600 to-zinc-400">SNEAKERS</span>  WITHOUT LIMITATION</span>
+            <span className="text-2xl font-bold text-white mx-20 lg:text-6xl">DISCOVER LIMITED <span className="text-transparent bg-clip-text bg-gradient-to-r from-gray-600 to-gray-400">SNEAKERS</span>  WITHOUT LIMITATION</span>
+            <span className="text-2xl font-bold text-white mx-20 lg:text-6xl">DISCOVER LIMITED <span className="text-transparent bg-clip-text bg-gradient-to-r from-gray-600 to-gray-400">SNEAKERS</span>  WITHOUT LIMITATION</span>
           </div>
         </div>
       </div>
@@ -46,13 +46,13 @@ export default function Home() {
           <h2 className="text-xl font-bold text-gray-900 sm:text-3xl">
             New Collection
           </h2>
-          <p className="max-w-md mx-auto mt-4 text-gray-500">
+          <p className="max-w-md mx-auto mt-4 text-gray-600">
             Do not miss the latest news in the world of sneakers, you can find all the new models in our store
           </p>
         </header>
         <ul className="grid grid-cols-1 gap-4 mt-8 lg:grid-cols-3">
           <li>
-            <a href="#" className="relative block group">
+            <Link to="/sneakers" className="relative block group">
               <img src="https://images.unsplash.com/photo-1618898909019-010e4e234c55?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80" alt="" className="object-cover w-full transition duration-500 aspect-square group-hover:opacity-90" />
               <div className="absolute inset-0 flex flex-col items-start justify-end p-6">
                 <h3 className="text-xl font-medium text-white">Casual Trainers</h3>
@@ -60,10 +60,10 @@ export default function Home() {
                   Shop Now
                 </span>
               </div>
-            </a>
+            </Link>
           </li>
           <li>
-            <a href="#" className="relative block group">
+            <Link to="/sneakers" className="relative block group">
               <img src="https://images.pexels.com/photos/2048548/pexels-photo-2048548.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" className="object-cover w-full transition duration-500 aspect-square group-hover:opacity-90" />
               <div className="absolute inset-0 flex flex-col items-start justify-end p-6">
                 <h3 className="text-xl font-medium text-white">Nike Air Force 1 Low</h3>
@@ -71,10 +71,10 @@ export default function Home() {
                   Shop Now
                 </span>
               </div>
-            </a>
+            </Link>
           </li>
           <li className="lg:col-span-2 lg:col-start-2 lg:row-span-2 lg:row-start-1">
-            <a href="#" className="relative block group">
+            <Link to="/sneakers" className="relative block group">
               <img src="https://erreizando.com/wp-content/uploads/2021/04/4k-uhd-background-blur-close-up-sneakers-shoes-legs-asphalt.jpg" alt="" className="object-cover w-full transition duration-500 aspect-square group-hover:opacity-90" />
               <div className="absolute inset-0 flex flex-col items-start justify-end p-6">
                 <h3 className="text-xl font-medium text-white">Nike Sneakers</h3>
@@ -82,7 +82,7 @@ export default function Home() {
                   Shop Now
                 </span>
               </div>
-            </a>
+            </Link>
           </li>
         </ul>
       </div>
@@ -97,7 +97,7 @@ export default function Home() {
               <span>Plus free shipping! Use code:</span>
               <span className="font-bold text-lg">SUMMER2023</span>
             </div>
-            <a href="#" rel="noreferrer noopener" className="px-5 mt-4 lg:mt-0 py-3 border block dark:bg-gray-50 dark:text-gray-900 dark:border-gray-400">Shop Now</a>
+            <Link to="/sneakers" rel="noreferrer noopener" className="px-5 mt-4 lg:mt-0 py-3 border block dark:bg-gray-50 dark:text-gray-900 dark:border-gray-400">Shop Now</Link>
           </div>
         </div>
       </div>
@@ -109,7 +109,7 @@ export default function Home() {
               <h2 className="text-4xl font-bold">Reviews</h2>
               <p className="dark:text-gray-400">The opinion of our clients is very important to us, we take it into account when improving our services.</p>
               <Link to={'/reviews'} className="mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium uppercase tracking-wide text-white">
-                See all reviews
+                Leave your review
               </Link>
             </div>
             <div className="p-6 xl:col-span-3">
@@ -120,7 +120,7 @@ export default function Home() {
                     <div className="flex items-center mt-4 space-x-4">
                       <img src="https://source.unsplash.com/50x50/?portrait?1" alt="" className="w-12 h-12 bg-center bg-cover rounded-full dark:bg-gray-500" />
                       <div>
-                        <p className="text-lg font-semibold">Leroy Jenkins</p>
+                        <p className="text-lg font-medium">Ifan Bryan</p>
                         <div className="flex items-center">
                           <svg aria-hidden="true" className="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>First star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
                           <svg aria-hidden="true" className="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Second star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
@@ -136,7 +136,7 @@ export default function Home() {
                     <div className="flex items-center mt-4 space-x-4">
                       <img src="https://source.unsplash.com/50x50/?portrait?2" alt="" className="w-12 h-12 bg-center bg-cover rounded-full dark:bg-gray-500" />
                       <div>
-                        <p className="text-lg font-semibold">Leroy Jenkins</p>
+                        <p className="text-lg font-medium">Lila Moore</p>
                         <div className="flex items-center">
                           <svg aria-hidden="true" className="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>First star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
                           <svg aria-hidden="true" className="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Second star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
@@ -154,7 +154,7 @@ export default function Home() {
                     <div className="flex items-center mt-4 space-x-4">
                       <img src="https://source.unsplash.com/50x50/?portrait?3" alt="" className="w-12 h-12 bg-center bg-cover rounded-full dark:bg-gray-500" />
                       <div>
-                        <p className="text-lg font-semibold">Leroy Jenkins</p>
+                        <p className="text-lg font-medium">Lilian Finch</p>
                         <div className="flex items-center">
                           <svg aria-hidden="true" className="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>First star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
                           <svg aria-hidden="true" className="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Second star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
@@ -173,7 +173,7 @@ export default function Home() {
                     <div className="flex items-center mt-4 space-x-4">
                       <img src="https://source.unsplash.com/50x50/?portrait?4" alt="" className="w-12 h-12 bg-center bg-cover rounded-full dark:bg-gray-500" />
                       <div>
-                        <p className="text-lg font-semibold">Leroy Jenkins</p>
+                        <p className="text-lg font-medium">Darren Klein</p>
                         <div className="flex items-center">
                           <svg aria-hidden="true" className="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>First star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
                           <svg aria-hidden="true" className="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Second star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
