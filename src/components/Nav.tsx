c54b0dd1302550d0f3d74aabefa72aa194ac86f4
Bautista Pietraroia <bautistapietraroia@gmail.com>
2023-03-18 16:48:16 -0300

    create burguer menu in nav component

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,8 +1,15 @@
 import { Link } from 'react-router-dom'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Logo from '../../public/sneakers.png'
+import User from './User'
+import MobileNav from './MobileNav'
 
 const Nav = () => {
+  const [menu, setMenu] = useState(false)
+  useEffect(() => {
+    if (!menu) document.body.style.overflow = 'unset'
+    else document.body.style.overflow = 'hidden'
+  }, [menu])
   return (
     <div>
       <div className="bg-black text-white flex items-center justify-center">
@@ -34,29 +41,36 @@ const Nav = () => {
               </span>
             </Link>
             {/* Sign In / Register      */}
-            <Link className="flex items-center hover:text-zinc-900" to="/login">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 hover:text-zinc-900" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-              </svg>
-            </Link>
+            <User></User>
           </div>
         </div>
         {/* Responsive navbar */}
-        <Link className="md:hidden flex mr-6 items-center" to="#">
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 hover:text-zinc-900" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-          </svg>
-          <span className="flex absolute -mt-5 ml-4">
-            <span className="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-pink-400 opacity-75" />
-            <span className="relative inline-flex rounded-full h-3 w-3 bg-pink-500">
+        <div className='flex gap-4 items-center'>
+          <Link className="md:hidden flex items-center" to="#">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 hover:text-zinc-900" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+            </svg>
+            <span className="flex absolute -mt-5 ml-4">
+              <span className="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-pink-400 opacity-75" />
+              <span className="relative inline-flex rounded-full h-3 w-3 bg-pink-500">
+              </span>
             </span>
-          </span>
-        </Link>
-        <Link className="navbar-burger self-center mr-12 md:hidden" to="">
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 hover:text-zinc-900" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
-          </svg>
-        </Link>
+          </Link>
+          <div className='md:hidden'>
+            <User></User>
+          </div>
+          <button onClick={() => setMenu(!menu)} className="navbar-burger self-center mr-12 md:hidden z-20">
+            {menu ?
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#000" className="bi bi-x-lg h-6 w-6" viewBox="0 0 16 16">
+                <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
+              </svg> :
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#000" className="bi bi-list h-6 w-6" viewBox="0 0 16 16">
+                <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
+              </svg>
+            }
+          </button>
+          {menu && <MobileNav setMenu={setMenu}></MobileNav>}
+        </div>
       </nav >
     </div >
 
